029325a344fab2762b895843dbc94904f2a9b655
pradyumna1979 <kuni.khadanga@gmail.com>
2023-04-15 22:46:19 +0530

    Atomic

--- src/MultiThreading/volatole/Volatile.java
+++ src/MultiThreading/volatole/Volatile.java
@@ -10,28 +10,28 @@ public class Volatile {
     public static void main(String[] args) throws InterruptedException {
         Counter counter = new Counter();
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        executorService.submit(()->{
-            Stream.iterate(1,p->p+1)
+        executorService.submit(()-> Stream.iterate(1, p->p+1)
+                .limit(1000)
+                .mapToInt(p->p)
+                .forEach(p->counter.increment()));
+        executorService.submit(()->Stream.iterate(1,p->p+1)
                     .limit(1000)
                     .mapToInt(p->p)
-                    .forEach(p->counter.increment());
-        });
-        executorService.submit(()->{
-            Stream.iterate(1,p->p+1)
-                    .limit(1000)
-                    .mapToInt(p->p)
-                    .forEach(p->counter.increment());
-        });
-        executorService.awaitTermination(1, TimeUnit.SECONDS);
+                    .forEach(p->counter.increment()));
+
+        boolean result = executorService.awaitTermination(1, TimeUnit.SECONDS);
+        System.out.println("result: "+result);
         System.out.println(counter.getValue());
     }
 }
+
 class Counter{
-    private AtomicInteger count = new AtomicInteger(0);
-    public int getValue(){
-        return count.get();
+    private final AtomicInteger atomicCount = new AtomicInteger(0);
+    private int i =0;
+    public void increment(){
+        i = atomicCount.incrementAndGet();
     }
-    public int increment(){
-        return count.getAndIncrement();
+    public  int getValue(){
+        return i;
     }
 }
